9aca8dee19e508af5a88a4a67cc80b9878c924fe
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-05-20 23:10:36 +0530

    Update meaning.py

--- jutsu/plugins/meaning.py
+++ jutsu/plugins/meaning.py
@@ -1,12 +1,13 @@
 import PyDictionary
 from pyrogram import Client, filters
 
+
 @Client.on_message(filters.command(["mng"]))
 async def meaning_(bot, message):
-    await message.edit("`Searching for meaning...`")
+    reply = await bot.send_message("`Searching for meaning...`")
     word = message.input_str or message.reply_to_message.text
     if not word:
-        await message.err("no input!")
+        await reply.err("no input!")
     else:
         dictionary = PyDictionary()
         words = dictionary.meaning(word)
@@ -16,6 +17,6 @@ async def meaning_(bot, message):
                 output = output + f"\n**{a}**\n"
                 for i in b:
                     output = output + f"â¾ __{i}__\n"
-            await message.edit(output)
+            await reply.edit(output)
         except Exception:
-            await message.err(f"Couldn't fetch meaning of {word}")
+            await reply.err(f"Couldn't fetch meaning of {word}")
